8619be67f98a9ae88d4e8ad7305e2de0142ff641
chenkai26 <chenkai26@baidu.com>
2024-11-01 20:10:06 +0800

    cpp build test

---
+++ test_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Copyright (c) 2024 Baidu.com, Inc. All Rights Reserved
+#
+"""
+Authors: chenkai26(chenkai26@baidu.com)
+Date:    2024/11/1
+"""
+import simple_lib
+
+print(simple_lib.greet("World"))  # 输出: Hello, World!
