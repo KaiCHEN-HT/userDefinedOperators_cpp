7e10feee23c577adad773a69ced5ff38e966015b
chenkai26 <chenkai26baidu.com>
2024-11-10 02:19:46 +0000

    build on linux and win64 using cmake

---
+++ myfunctions_declspec_cmakeCompile/build_linux/CMakeFiles/myfunctions.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myfunctions.dir/myfunctions_linux.cpp.o"
+  "libmyfunctions.pdb"
+  "libmyfunctions.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myfunctions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
