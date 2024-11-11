b069053c3ae4a80713ea97f251e46059facfc994
chenkai26 <chenkai26@baidu.com>
2024-11-11 12:02:34 +0800

    call compiled C++ func with python-standard-lib ctypes

--- myfunctions_declspec_cmakeCompile/myfunctions_win64.py
+++ myfunctions_declspec_cmakeCompile/myfunctions_win64.py
@@ -7,26 +7,29 @@
 Authors: chenkai26(chenkai26@baidu.com)
 Date:    2024/9/12
 """
-import ctypes
+from ctypes import WinDLL, c_int, c_char_p, CFUNCTYPE
 
 # 加载DLL
 # myfunctions_dll = ctypes.WinDLL('./myfunctions.dll')
-myfunctions_dll = ctypes.WinDLL('./build_win64/Release/myfunctions.dll')
+myfunctions_dll = WinDLL('./build_win64/Release/myfunctions.dll')
 
-# 调用 add 函数
+# 封装 add 函数
 add = myfunctions_dll.add
-add.restype = ctypes.c_int
-add.argtypes = [ctypes.c_int, ctypes.c_int]
-result = add(333, 40)
-print(f"3 + 40 = {result}")
+add.restype = c_int
+add.argtypes = [c_int, c_int]
 
-# 调用 get_message 函数
+# 封装 get_message 函数
 get_message = myfunctions_dll.get_message
-get_message.restype = ctypes.c_char_p  # 设置返回类型为 char*
-message = get_message()  # 获得消息并打印
-print(ctypes.c_char_p(message).value.decode('utf-8'))
+get_message.restype = c_char_p  # 设置返回类型为 char*
 
-# 调用 free_message 函数以释放内存
+# 封装 free_message 函数以释放内存
 free_message = myfunctions_dll.free_message
-free_message.argtypes = [ctypes.c_char_p]  # 设置参数类型
-free_message(message)  # 释放内存
+free_message.argtypes = [c_char_p]  # 设置参数类型
+
+
+if __name__ == '__main__':
+    result = add(333, 40)
+    print(f"333 + 40 = {result}")
+    message = get_message()  # 获得消息并打印
+    print(c_char_p(message).value.decode('utf-8'))
+    # free_message(message)  # 释放内存
